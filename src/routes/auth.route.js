16390efff7f9c59b025d4195dae410bff61904b2
Lourdes-s <lou_santillan98@hotmail.com>
2024-12-21 12:50:04 -0300

    fix

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { forgotPasswordController, loginController, registerController, verifyEmailController} from '../controllers/auth.controllers.js'
+import { forgotPasswordController, loginController, registerController, verifyEmailController, recoveryPasswordController} from '../controllers/auth.controllers.js'
 
 const authRouter = express.Router()
 
@@ -7,5 +7,6 @@ authRouter.post('/register', registerController)
 authRouter.post('/login', loginController)
 authRouter.get('/verify-email/:validation_token', verifyEmailController)
 authRouter.post('/forgot-password', forgotPasswordController)
+authRouter.post('/recovery-password', recoveryPasswordController)
 
 export default authRouter
