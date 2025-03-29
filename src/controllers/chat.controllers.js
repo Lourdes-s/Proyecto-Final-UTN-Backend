92c320e642fbf3e26d3c8a3b364031559c7b5283
Lourdes.s <lou_santillan98@hotmail.com>
2025-03-29 13:23:39 -0300

    fix

--- src/controllers/chat.controllers.js
+++ src/controllers/chat.controllers.js
@@ -21,7 +21,7 @@ export const getChatController = async (req, res, next) => {
 
         //TODO maybe check if req.user.id is contact with user_id_contact
 
-        const messages = await ChatRepository.getChat(req.user.id, user_id_contact, Number(page), Number(per_pag))
+        const messages = await ChatRepository.getChat(req.user.id, user_id_contact, Number(page), Number(per_page))
 
         return res.status(200).json(messages)
     }
